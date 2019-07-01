c3286433cd6500c3b8405d8c8deea818e68f8256
gsnyder206 <gsnyder@stsci.edu>
2019-07-01 13:34:20 -0400

    add example jupyter notebook

--- pristine_to_mock.py
+++ pristine_to_mock.py
@@ -28,6 +28,27 @@ def output_pristine_fits_image(original_image,out_image):
 
 def convolve_with_fwhm(in_image, out_image, fwhm_arcsec=0.10):
 
+    #open pristine image fits file
+    in_fo=fits.open(in_image,'readonly')
+
+    #load image data and metadata
+    image_in=in_fo['SimulatedImage'].data
+    header_in=in_fo['SimulatedImage'].header
+
+    #calculate PSF width in pixel units
+    pixel_size_arcsec=header_in['PIXSCALE']
+    sigma_arcsec=fwhm_arcsec*gaussian_fwhm_to_sigma
+    sigma_pixels=sigma_arcsec/pixel_size_arcsec
+    
+    image_out=sp.ndimage.filters.gaussian_filter(image_in,sigma_pixels,mode='nearest')
+    hdu_out = fits.ImageHDU(image_out,header=header_in)
+    hdu_out.header['FWHMPIX']=(sigma_pixels/gaussian_fwhm_to_sigma,'pixels')
+    hdu_out.header['FWHM']=(fwhm_arcsec,'arcsec')
+    hdu_out.header['SIGMAPIX']=(sigma_pixels,'pixels')
+    hdu_out.header['SIGMA']=(sigma_arcsec,'arcsec')
+    hdu_out.header['EXTNAME']='MockImage_Noiseless'
+
+    
     return
 
 
