496d73a311accfa01a5fa8ff67cedc641c41c5e0
gsnyder206 <gsnyder@stsci.edu>
2019-07-01 17:28:49 -0400

    placeholder analysis routines

---
+++ analyze_mock.py
@@ -0,0 +1,39 @@
+#code to do source detection, segmentation, and morphology measurements from simple mock images
+import astropy
+import astropy.io.ascii as ascii
+import astropy.io.fits as fits
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as pyplot
+import astropy.units as u
+import glob
+import sys
+import os
+import photutils
+from astropy.stats import gaussian_fwhm_to_sigma
+import scipy.ndimage
+import scipy as sp
+
+
+def detect_sources():
+
+
+
+    return
+
+
+
+def deblend_sources():
+
+
+
+    return
+
+
+
+def run_statmorph():
+
+
+
+    return
+
