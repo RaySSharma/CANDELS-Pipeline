f46c70e1e90c31e605acfbd3c28dd709aa54e344
gsnyder206 <gsnyder@stsci.edu>
2019-07-02 09:25:57 -0400

    source detection and segmentation draft

--- analyze_mock.py
+++ analyze_mock.py
@@ -11,20 +11,97 @@ import sys
 import os
 import photutils
 from astropy.stats import gaussian_fwhm_to_sigma
+from astropy.convolution import Gaussian2DKernel
 import scipy.ndimage
 import scipy as sp
 
 
-def detect_sources():
+def detect_sources(in_image,ext_name='MockImage_SB25',**kwargs):
+
+    try:
+        this_fo=fits.open(in_image,'append')
+        this_hdu=this_fo[ext_name]
+    except:
+        print('HDU not found! ', in_image, ext_name)
+        return
+    
+    rms=this_hdu.header['RMSNOISE']
+    thr_rms = 1.3
+    thresh = thr_rms * rms
+    npixels = 10
+
+    #build kernel for pre-filtering.  How big?
+    #don't assume redshift knowledge here
+    typical_kpc_per_arcsec = 8.0
+
+    #abzp = image_hdu.header['ABZP']
+
+    kernel_kpc_fwhm = 5.0
+    kernel_arcsec_fwhm = kernel_kpc_fwhm/typical_kpc_per_arcsec
+    kernel_pixel_fwhm = kernel_arcsec_fwhm/this_hdu.header['PIXSIZE']
+
+    sigma = kernel_pixel_fwhm * gaussian_fwhm_to_sigma
+    nsize = int(5*kernel_pixel_fwhm)
+    kernel = Gaussian2DKernel(sigma, x_size=nsize, y_size=nsize)
+
+    #may consider inputting an error image here -- can be computed with photutils plus a GAIN keyword -- ratio of flux units to counts
+    
+    segmap_obj = photutils.detect_sources(this_hdu.data, thresh, npixels=npixels, filter_kernel=kernel, **kwargs)
+    segmap = segmap_obj.data
+    props = photutils.source_properties(this_hdu.data, segmap)
+
+    props_table=astropy.table.Table(props.to_table())
+    #these give problems given their format/NoneType objects
+    props_table.remove_columns(['sky_centroid','sky_centroid_icrs','source_sum_err','background_sum','background_mean','background_at_centroid'])
+    
+    #save segmap and info
+    nhdu = fits.ImageHDU(np.int32(segmap))
+    nhdu.header['EXTNAME']='SEGMAP'
+
+    thdu = fits.BinTableHDU(props_table)
+    thdu.header['EXTNAME']='SEGMAP_PROPS'
+    
+    this_fo.append(nhdu)
+    this_fo.append(thdu)
+    
+    this_fo.flush()
+
+    return segmap_obj, kernel
 
 
 
-    return
+def deblend_sources(in_image,segm_obj,kernel,ext_name='MockImage_SB25',**kwargs):
 
+    try:
+        this_fo=fits.open(in_image,'append')
+        this_hdu=this_fo[ext_name]
+    except:
+        print('HDU not found! ', in_image, ext_name)
+        return
 
+    
+    segm_deblend = photutils.deblend_sources(this_hdu.data, segm_obj, npixels=10,
+                                             filter_kernel=kernel, nlevels=32,
+                                             contrast=0.001)
+    segmap = segm_deblend.data
+    props = photutils.source_properties(this_hdu.data, segmap)
 
-def deblend_sources():
+    props_table=props.to_table()
+    #these give problems given their format/NoneType objects
+    props_table.remove_columns(['sky_centroid','sky_centroid_icrs','source_sum_err','background_sum','background_mean','background_at_centroid'])
+    
+    #save segmap and info
+    nhdu = fits.ImageHDU(np.int32(segmap))
+    nhdu.header['EXTNAME']='DEBLEND'
 
+    thdu = fits.BinTableHDU(props_table)
+    thdu.header['EXTNAME']='DEBLEND_PROPS'
+    
+    
+    this_fo.append(nhdu)
+    this_fo.append(thdu)
+    
+    this_fo.flush()
 
 
     return
