b8a40a70939f90f2c0c9b43a2779b393a032934a
rayssharma <ramon.sharma1@gmail.com>
2019-11-11 16:33:29 -0500

    Feature: Now packages morphology + halo + BH data together.

--- package_mock_images.py
+++ package_mock_images.py
@@ -1,21 +1,113 @@
-import astropy.io.fits as fits
 import glob
 import os
+import numpy as np
+import pandas as pd
+import astropy.io.fits as fits
 
-BH_model = 'Hopkins'
-SB_limit = '25'
+SB_LIMIT = '25'
+BH_MODEL = 'Nenkova'
+OVERWRITE = True
 
-image_loc = '/home/rss230/AGN-Obscuration/outputs/BH+' + BH_model + '/*/*/WFC3_F160W/*/'
-image_files = glob.glob(image_loc + '*.image.SB' + SB_limit + '.fits')
-output_dir = '/scratch/rss230/Kocevski_images/'
+IMAGE_LOC = '/scratch/rss230/AGN-Obscuration/outputs/*/*/WFC3_F160W/0/'
+IMAGE_FILES = glob.glob(IMAGE_LOC + '*.image.SB' + SB_LIMIT + '.fits')
+IMAGE_OUTPUT_DIR = '/scratch/rss230/Kocevski_images/'
+DATA_OUTPUT = '/home/rss230/AGN-Obscuration/outputs/data_SB' + SB_LIMIT + '.h5'
+MORPH_PARAMS = {
+    'GINI': 'gini',
+    'M20': 'm20',
+    'CONC': 'concentration',
+    'ASYM': 'asymmetry',
+    'SMOOTH': 'smoothness',
+    'SERSIC_N': 'sersic_n',
+    'M': 'multimode',
+    'D': 'deviation',
+    'I': 'intensity',
+    'FLAG': 'flag',
+    'FLAG_SERSIC': 'flag_sersic'
+}
+MISSING_BH_SED = 0
 
-for i, image in enumerate(image_files):
+
+def save_hdu(hdu, filename):
+    count = 0
+    while True:
+        filename = filename[:-7] + '.' + str(count) + '.fits'
+        try:
+            hdu.writeto(filename, overwrite=OVERWRITE)
+            break
+        except OSError:
+            count += 1
+    print('Output image:', filename)
+
+
+def gather_data(im, morph, z):
+    global MISSING_BH_SED
+    try:
+        with np.load(os.path.dirname(im) + '/bh_sed.npz') as bh_sed:
+            lum = bh_sed['luminosity'].sum()
+            fnu = bh_sed['fnu']
+            nu = bh_sed['nu']
+            lum_obsc = -np.sum(np.trapz(fnu / nu, nu, axis=1))
+    except FileNotFoundError:
+        print('No bh_sed.npz found.')
+        MISSING_BH_SED += 1
+        lum = np.nan
+        lum_obsc = np.nan
+
+    halo_properties = im.split('/')[-1].split('.')
+    halo_num = halo_properties[0]
+    timestep = halo_properties[1]
+    filter_name = halo_properties[2]
+    SB = halo_properties[4]
+    morph_params = gather_morph_params(morph)
+    image_data = np.hstack([
+        halo_num, timestep, z, filter_name, SB, BH_MODEL, lum, lum_obsc,
+        morph_params
+    ])
+    return image_data
+
+
+def gather_morph_params(morph):
+    params = []
+    for par in MORPH_PARAMS:
+        try:
+            params.append(morph[par])
+        except KeyError:
+            params.append(np.nan)
+    return params
+
+
+packaged_data = []
+for i, image in enumerate(IMAGE_FILES):
     print('Image:', image)
-    print('Image number:', i, '/', len(image_files))
+    print('Image number:', i, '/', len(IMAGE_FILES))
     with fits.open(image) as f:
-        output_filename = output_dir + os.path.basename(image)
-        print('Output image:', output_filename)
+        output_filename = IMAGE_OUTPUT_DIR + os.path.basename(
+            image)[:-5] + '.0.fits'
         data = f['MockImage'].data
         header = f['MockImage'].header
+        redshift = header['REDSHIFT']
+        morphology = f['SOURCE_MORPH'].header
+
         f_out = fits.PrimaryHDU(data=data, header=header)
-        f_out.writeto(output_filename, overwrite=True)
+
+        save_hdu(f_out, output_filename)  # Save mock HDU
+
+        packaged_data.append(
+            gather_data(image, morphology,
+                        redshift))  # Gather morphological + halo + BH data
+
+columns = [
+    'halo_num', 'timestep', 'redshift', 'filter', 'SB', 'BH_model', 'Lbol',
+    'Lbol_obsc'
+] + list(MORPH_PARAMS.keys())
+packaged_data = np.array(packaged_data)
+df2 = pd.DataFrame(packaged_data, columns=columns)
+
+if not os.path.isfile(DATA_OUTPUT):
+    df1 = pd.DataFrame()
+else:
+    df1 = pd.read_hdf(DATA_OUTPUT, key='data')
+
+df = pd.concat([df1, df2])
+df.to_hdf(DATA_OUTPUT, key='data')
