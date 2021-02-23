8e92ffb1119791c283a8b8d517421b6a2c37f6cd
RaySSharma <ramon.sharma1@gmail.com>
2021-02-23 09:13:47 -0500

    Added RealSim Bottrell+(2019)

--- pristine_to_mock.py
+++ pristine_to_mock.py
@@ -13,7 +13,7 @@ from astropy.stats import gaussian_fwhm_to_sigma
 
 def output_pristine_fits_image(image_file, out_file, filt_wheel):
     # Input image in units lambda*F_lambda [erg/s/cm^2]
-    fo = fits.open(image_file, 'readonly')
+    fo = fits.open(image_file, "readonly")
 
     outfo = fits.HDUList()
     outfo.append(fo[0])
@@ -22,7 +22,7 @@ def output_pristine_fits_image(image_file, out_file, filt_wheel):
     header = fo[0].header
 
     # Select filter wheel
-    filt = filt_wheel[header['FILTER']]
+    filt = filt_wheel[header["FILTER"]]
     lambda_eff = filt[0]
 
     lambda_eff_microns = lambda_eff * u.micron
@@ -32,12 +32,12 @@ def output_pristine_fits_image(image_file, out_file, filt_wheel):
     image_flux = (im * u.erg / (u.s * u.cm * u.cm)) / (nu_eff_hz)
 
     # Convert to NanoJanskies
-    image_njy = image_flux.to('nJy')
+    image_njy = image_flux.to("nJy")
 
     image_hdu = fits.ImageHDU(image_njy.value, header=header)
 
-    image_hdu.header['EXTNAME'] = 'SimulatedImage'
-    image_hdu.header['BUNIT'] = 'nanojanskies'
+    image_hdu.header["EXTNAME"] = "SimulatedImage"
+    image_hdu.header["BUNIT"] = "nanojanskies"
 
     outfo.append(image_hdu)
     outfo.writeto(out_file, overwrite=True)
@@ -48,39 +48,39 @@ def output_pristine_fits_image(image_file, out_file, filt_wheel):
 # Convolve image with diffraction-limited PSF FWHM
 def convolve_with_fwhm(in_image, filt_wheel):
     # open pristine image fits file
-    in_fo = fits.open(in_image, 'append')
+    in_fo = fits.open(in_image, "append")
 
     # load image data and metadata
-    image_in = in_fo['SimulatedImage'].data
-    header_in = in_fo['SimulatedImage'].header
+    image_in = in_fo["SimulatedImage"].data
+    header_in = in_fo["SimulatedImage"].header
 
     # Select filter wheel
-    filt = filt_wheel[header_in['FILTER']]
+    filt = filt_wheel[header_in["FILTER"]]
 
     # Set diffraction-limited FWHM, in arcseconds
     fwhm_arcsec = filt[1]
 
     # Cosmology used by Choi et al. 2018
-    redshift = header_in['REDSHIFT']
+    redshift = header_in["REDSHIFT"]
     cosmology = astropy.cosmology.FlatLambdaCDM(0.72 * 100.0, 0.26, Ob0=0.044)
     kpc_per_arcsec = cosmology.kpc_proper_per_arcmin(redshift).value / 60.0
 
     # calculate PSF width in pixel units
-    pixel_size_arcsec = 2 * header_in['PIX_KPC'] / kpc_per_arcsec
+    pixel_size_arcsec = 2 * header_in["PIX_KPC"] / kpc_per_arcsec
     sigma_arcsec = fwhm_arcsec * gaussian_fwhm_to_sigma
     sigma_pixels = sigma_arcsec / pixel_size_arcsec
 
-    image_out = sp.ndimage.filters.gaussian_filter(image_in, sigma_pixels,
-                                                   mode='nearest')
+    image_out = sp.ndimage.filters.gaussian_filter(
+        image_in, sigma_pixels, mode="nearest"
+    )
 
     hdu_out = fits.ImageHDU(image_out, header=header_in)
-    hdu_out.header['FWHMPIX'] = (sigma_pixels / gaussian_fwhm_to_sigma,
-                                 'pixels')
-    hdu_out.header['SIGMAPIX'] = (sigma_pixels, 'pixels')
-    hdu_out.header['FWHM'] = (fwhm_arcsec, 'arcsec')
-    hdu_out.header['SIGMA'] = (sigma_arcsec, 'arcsec')
-    hdu_out.header['EXTNAME'] = 'MockImage_Noiseless'
-    hdu_out.header['PIXSIZE'] = (pixel_size_arcsec, 'arcsec')
+    hdu_out.header["FWHMPIX"] = (sigma_pixels / gaussian_fwhm_to_sigma, "pixels")
+    hdu_out.header["SIGMAPIX"] = (sigma_pixels, "pixels")
+    hdu_out.header["FWHM"] = (fwhm_arcsec, "arcsec")
+    hdu_out.header["SIGMA"] = (sigma_arcsec, "arcsec")
+    hdu_out.header["EXTNAME"] = "MockImage_Noiseless"
+    hdu_out.header["PIXSIZE"] = (pixel_size_arcsec, "arcsec")
 
     in_fo.append(hdu_out)
     in_fo.flush()
@@ -90,16 +90,21 @@ def convolve_with_fwhm(in_image, filt_wheel):
 
 
 # accepts sb_maglim value which corresponds to magnitudes per square arcsecond
-def add_simple_noise(in_image, sb_maglim, ext_name, alg='Snyder2019'):
+def add_simple_noise(in_image, sb_maglim, ext_name, alg="Snyder2019"):
 
-    in_fo = fits.open(in_image, 'append')
-    image_in = in_fo['MockImage_Noiseless'].data
-    header_in = in_fo['MockImage_Noiseless'].header
+    in_fo = fits.open(in_image, "append")
+    image_in = in_fo["MockImage_Noiseless"].data
+    header_in = in_fo["MockImage_Noiseless"].header
 
-    if alg == 'Snyder2019':  # algorithm from Snyder et al. (2019), SB_limit \sim 5sigma above the background
-        sigma_njy = (2.0**(-0.5)) * (
-            (1.0e9) * (3631.0 / 5.0) * 10.0**(-0.4 * sb_maglim)
-        ) * header_in['PIXSIZE'] * (3.0 * header_in['FWHM'])
+    if (
+        alg == "Snyder2019"
+    ):  # algorithm from Snyder et al. (2019), SB_limit \sim 5sigma above the background
+        sigma_njy = (
+            (2.0 ** (-0.5))
+            * ((1.0e9) * (3631.0 / 5.0) * 10.0 ** (-0.4 * sb_maglim))
+            * header_in["PIXSIZE"]
+            * (3.0 * header_in["FWHM"])
+        )
     else:
         sigma_njy = 0
 
@@ -110,61 +115,12 @@ def add_simple_noise(in_image, sb_maglim, ext_name, alg='Snyder2019'):
     image_out = image_in + noise_image
 
     hdu_out = fits.ImageHDU(image_out, header=header_in)
-    hdu_out.header['EXTNAME'] = ext_name
-    hdu_out.header['SBLIM'] = (sb_maglim, 'mag/arcsec^2')
-    hdu_out.header['RMSNOISE'] = (sigma_njy, 'nanojanskies')
+    hdu_out.header["EXTNAME"] = ext_name
+    hdu_out.header["SBLIM"] = (sb_maglim, "mag/arcsec^2")
+    hdu_out.header["RMSNOISE"] = (sigma_njy, "nanojanskies")
 
     in_fo.append(hdu_out)
     in_fo.flush()
     in_fo.close()
 
-    return
-
-
-def make_galaxies_astropy(image_file, flux, galsize, xi, yi, ar, pa, n=4):
-    # Written by Steven Boada (2019)
-    from astropy.modeling.models import Sersic1D, Sersic2D
-
-    fo = fits.open(image_file, 'update')
-
-    image = fo['SimulatedImage'].data
-    header = fo['SimulatedImage'].header
-
-    fluxlim = 0.0001 * flux  # 0.1
-    scale = 0.5  # arcsec/pixel
-    r_e = galsize  # effective radius
-    ellip = ar  # ellipticity
-    theta = np.deg2rad(pa)  # position angle
-    x_cent = 0  # x centroid
-    y_cent = 0  # x centroid
-    tot_flux = flux  # total flux
-
-    s1 = Sersic1D(amplitude=1, r_eff=r_e, n=n)
-    r = np.arange(0, 1000, scale)
-    s1_n = s1(r) / sum(s1(r))
-    extent = np.where(s1_n * flux > fluxlim)[0].max()
-
-    if extent % 2 > 0:
-        extent += 1
-
-    ser_model = Sersic2D(r_eff=r_e, n=n, ellip=ellip, theta=theta, x_0=x_cent,
-                         y_0=y_cent)
-
-    x = np.arange(-extent / 1., extent / 1., scale) + x_cent / scale
-    y = np.arange(-extent / 1., extent / 1., scale) + y_cent / scale
-
-    X, Y = np.meshgrid(x, y)
-
-    img = ser_model(X, Y)
-    img /= np.sum(img)
-    img *= tot_flux
-
-    xi, yi = int(xi), int(yi)
-    # COLUMNS FIRST -- because FITS are silly
-    print(image.shape)
-    image[yi - img.shape[1] // 2:yi + img.shape[1] // 2, xi -
-          img.shape[0] // 2:xi + img.shape[0] // 2] += img
-
-    fo['SimulatedImage'].data = image
-    fo.writeto(image_file, overwrite=True)
     return
--- run_pipeline.py
+++ run_pipeline.py
@@ -1,71 +1,107 @@
 #!/usr/bin/env python3
 import pristine_to_mock as ptm
 import analyze_mock as am
+import RealSim.ObsRealism as obs
 import glob
 import matplotlib
-matplotlib.use('Agg')
+
+matplotlib.use("Agg")
 
 DEBLEND = True
 
 # {filter: central wavelength [micron], instrument resolution [arcsec], gain}
 filt_wheel = {
-    'ACS_F814W': [0.8353, 0.124, 1.55],
-    'WFC3_F160W': [1.5369, 0.145, 2.4],
-    'ACS_F606W': [0.5907, 0.124, 1.55]
+    "ACS_F814W": [0.8353, 0.124, 1.55],
+    "WFC3_F160W": [1.5369, 0.145, 2.4],
+    "ACS_F606W": [0.5907, 0.124, 1.55],
 }
+# CANDELS RealSim catalog/field parameters
+field_info = {
+    "UDS": [
+        "hlsp_candels_hst_wfc3_uds-tot-multiband_f160w_v1_cat.fits",
+        "hlsp_candels_hst_wfc3_uds-tot_f160w_v1.0_drz.fits",
+    ],
+    "EGS": [
+        "hlsp_candels_hst_wfc3_egs-tot-multiband_f160w_v1_cat.fits",
+        "hlsp_candels_hst_wfc3_egs-tot-60mas_f160w_v1.0_drz.fits",
+    ],
+    "GS": [
+        "hlsp_candels_hst_wfc3_goodss-tot-multiband_f160w_v1_cat.fits",
+        "hlsp_candels_hst_wfc3_gs-tot_f160w_v1.0_drz.fits",
+    ],
+    "GN": [
+        "hlsp_candels_hst_wfc3_goodsn-barro19_multi_v1-1_photometry-cat.fits",
+        "hlsp_candels_hst_wfc3_gn-tot-60mas_f160w_v1.0_drz.fits",
+    ],
+    "COS": [
+        "hlsp_candels_hst_wfc3_cos-tot-multiband_f160w_v1_cat.fits",
+        "hlsp_candels_hst_wfc3_cos-tot_f160w_v1.0_drz.fits",
+    ],
+}
+realsim_input_dir = "/home/ray/Research/CANDELS-Pipeline/RealSim/Inputs/"
+
 # mag/arcsec^2
 detection_limits = [25, 27]
 
 # List of morphological parameters to calculate in statmorph
 morph_params = {
-    'GINI': 'gini',
-    'M20': 'm20',
-    'CONC': 'concentration',
-    'ASYM': 'asymmetry',
-    'SMOOTH': 'smoothness',
-    'SERSIC_N': 'sersic_n',
-    'M': 'multimode',
-    'D': 'deviation',
-    'I': 'intensity',
-    'FLAG': 'flag',
-    'FLAG_SERSIC': "flag_sersic"
+    "GINI": "gini",
+    "M20": "m20",
+    "CONC": "concentration",
+    "ASYM": "asymmetry",
+    "SMOOTH": "smoothness",
+    "SERSIC_N": "sersic_n",
+    "M": "multimode",
+    "D": "deviation",
+    "I": "intensity",
+    "FLAG": "flag",
+    "FLAG_SERSIC": "flag_sersic",
 }
-image_loc = '/scratch/rss230/AGN-Obscuration/outputs/*/*/WFC3_F160W/[3-5]/'
-image_files = glob.glob(image_loc + '*.image.fits')
+image_loc = "/scratch/rss230/AGN-Mergers/outputs/*/*/WFC3_F160W/[0-2]/"
+image_files = glob.glob(image_loc + "*.image.fits")
 
 for i, image in enumerate(image_files):
-    print('Image Number:', i, '/', len(image_files), flush=True)
-    print('Image:', image, flush=True)
+    print("Image Number:", i, "/", len(image_files), flush=True)
+    print("Image:", image, flush=True)
     for lim in detection_limits:
-        image_mock = image[:-5] + '.SB' + str(lim) + '.fits'
-        fig_name = image[:-5] + '.SB' + str(lim) + '.png'
-        ext_name = 'MockImage'
+        image_mock = image[:-5] + ".SB" + str(lim) + ".fits"
+        fig_name = image[:-5] + ".SB" + str(lim) + ".png"
+        ext_name = "MockImage"
 
-        print('SB:', lim, 'mag arcsec^-2', flush=True)
-        print('Mock Image:', image_mock, flush=True)
+        print("SB:", lim, "mag arcsec^-2", flush=True)
+        print("Mock Image:", image_mock, flush=True)
 
         ptm.output_pristine_fits_image(
-            image, image_mock, filt_wheel)  # Setup image for mock creation
-        ptm.convolve_with_fwhm(image_mock,
-                               filt_wheel)  # Convolve mock image with PSF
-        ptm.add_simple_noise(image_mock, sb_maglim=lim, ext_name=ext_name,
-                             alg='Snyder2019')  # Add noise model
+            image, image_mock, filt_wheel
+        )  # Setup image for mock creation
+        ptm.convolve_with_fwhm(image_mock, filt_wheel)  # Convolve mock image with PSF
+        ptm.add_simple_noise(
+            image_mock, sb_maglim=lim, ext_name=ext_name, alg="Snyder2019"
+        )  # Add noise model
+
+        candels_args = obs.make_candels_args(field_info, realsim_input_dir)
+        new_im, real_im = obs.ObsRealism(
+            image_mock, candels_args
+        )  # Apply RealSim CANDELS fields
 
         seg, kernel, errmap = am.detect_sources(
-            image_mock,
-            ext_name=ext_name, filt_wheel=filt_wheel)  # Run source detection with photutils
+            image_mock, ext_name=ext_name, filt_wheel=filt_wheel
+        )  # Run source detection with photutils
 
         if DEBLEND:
             seg = am.deblend_sources(
-                image_mock, seg, kernel, errmap,
-                ext_name=ext_name)  # Deblend detected sources
-            props_ext_name = 'DEBLEND_PROPS'
+                image_mock, seg, kernel, errmap, ext_name=ext_name
+            )  # Deblend detected sources
+            props_ext_name = "DEBLEND_PROPS"
         else:
-            props_ext_name = 'SEGMAP_PROPS'
+            props_ext_name = "SEGMAP_PROPS"
 
         source_morph = am.source_morphology(
-            image_mock, seg, errmap=errmap, ext_name=ext_name,
-            props_ext_name=props_ext_name
+            image_mock,
+            seg,
+            errmap=errmap,
+            ext_name=ext_name,
+            props_ext_name=props_ext_name,
         )  # Calculate morphological parameters using statmorph
 
         am.save_morph_params(
